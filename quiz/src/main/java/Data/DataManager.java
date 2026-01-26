7e4019e45b4a97b05df72abc3e85422b1116e8c9
maduro76 <dominik.michalak76@gmail.com>
2026-01-26 21:13:51 +0100

    Fixed generics

--- quiz/src/main/java/Data/DataManager.java
+++ quiz/src/main/java/Data/DataManager.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface DataManager<T> {
-    List<Quiz<String>> loadAll();
+    List<T> loadAll();
     void saveAll(List<T> items);
     Optional<T> findById(String id);
     void addItems(T item);
